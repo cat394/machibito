9360916beb86ed4e9ddeacbcd61e959feae778fa
Ryo <keyring1231@gmail.com>
2023-08-16 10:57:55 +0900

    名前のミスを変更した

--- src/data/audio.ts
+++ src/data/audio.ts
@@ -12,7 +12,7 @@ export const audio: Audio = {
         { name: "bell", alt: "神社で鳴らす鐘の音", audioTitle: "鈴を鳴らす" },
         { name: "osaisen", alt: "お賽銭音", audioTitle: "硬貨を入れる" },
         { name: "autumngarden_with_rain", alt: "秋風吹く庭で(雨音入り)", audioTitle: "BGM" },
-        { name: "Unforgettable_Dream_Tale", alt: "忘れられない夢の思い出", audioTitle: "BGM"}
+        { name: "Unforgetable_Dream_Tale", alt: "忘れられない夢の思い出", audioTitle: "BGM"}
       ]
     }
   ]
