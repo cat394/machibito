4d9b426a6558e61ecf737635e0f71be6a7f94558
Ryo <keyring1231@gmail.com>
2023-08-16 16:24:55 +0900

    formが機能しないメッセージを追加した。

--- src/components/form/Form.svelte
+++ src/components/form/Form.svelte
@@ -2,8 +2,14 @@
   import Input from "./Input.svelte";
   import Textarea from "./Textarea.svelte";
   import SubmitButton from "./SubmitButton.svelte";
+  import StickyNote from "../stickyNote/StickyNote.svelte";
+  import Spacer from "../spacer/Spacer.svelte";
 </script>
 
+<StickyNote type="warn">
+  <p>現在（2023/08/16）において、フォームが機能しない不具合が発生しているため、原因が解決するまでの間、お手数ですがページ下部に記載されているSNSアカウントよりご連絡ください。</p>
+</StickyNote>
+<Spacer />
 <form 
   class="grid gap-10" 
   name="contact"
