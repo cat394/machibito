2b6662872d9e353cd01c98d9d27109c1fe234fbc
Ryo <keyring1231@gmail.com>
2023-08-19 12:03:50 +0900

    reactを削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), react()],
+  integrations: [svelte(), tailwind()],
   experimental: {
     viewTransitions: true
   },
